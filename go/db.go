ad2ae8f8899e261f6fd8a14e7e150240d3169a89
jsz <jszpilewski@me.com>
2011-04-11 10:22:58 +0200

    news and today commands

--- go/db.go
+++ go/db.go
@@ -8,7 +8,7 @@ type BlogDB interface {
 	StorePost(post *BlogPost) (int64, os.Error)
 	GetPost(post_id int64) (BlogPost, os.Error)
 	GetPostsForTimespan(start_timestamp, end_timestamp int64) (posts []BlogPost, err os.Error)
-	GetLastNPosts(num_to_get int) (posts []BlogPost, err os.Error)
+	GetLastNPosts(num_to_get int32) (posts []BlogPost, err os.Error)
 	
 	StoreComment(comment *PostComment) (int64, os.Error)
     GetComments(post_id int64) (comments []PostComment, err os.Error);
