0d040259cc0c9cc2b98cab2b73844ed1b842a1b5
jsz <jszpilewski@me.com>
2011-04-13 17:11:44 +0200

    added htmlstrip

---
+++ go/htmlstrip.go
@@ -0,0 +1,52 @@
+package main
+
+/*import (
+	"fmt"
+)*/
+
+//I hope I got the slices right and am not copying strings aroung :]
+func htmlstrip(s string) string {
+	bytes := []byte(s)
+	l := len(s)
+	ts := make([]byte, l)
+	sl := bytes[:]
+	
+	tmp := 0
+	for i, c := range s {
+		if c == '<' {
+			idx := i
+			if idx < 0 {
+				idx = 0
+			}
+			ts = append(ts, sl[tmp:idx]...)
+			tmp = idx
+		}
+		if c == '>' {
+			tmp = i+1
+			if tmp > l {
+				tmp = l
+			}
+		}
+	}
+	ts = append(ts, sl[tmp:len(s)]...)
+	
+	outstr := string(ts)
+	return outstr
+}
+
+/*
+var x = `Living the future:
+<blockquote>Die US-Marine hat erstmals einen Hochenergie-Laser auf See abgefeuert - und bei dem Experiment ein kleines Boot in Brand gesetzt. Schiffe sollen sich künftig mit solchen Energiewaffen verteidigen</blockquote>
+(<a href="http://www.spiegel.de/wissenschaft/technik/0,1518,756514,00.html">via</a>)
+`
+
+func main() {
+	strs := []string{"<b>ich bin behinat</b>.",
+		"<a href='http://www.de'>lol</a>",
+		"das ist ein <p>test</p>",
+		x,
+		"omg <b>d</b> lol<br>o"}
+	for _, s := range strs {
+		fmt.Println(htmlstrip(s))
+	}
+}*/
