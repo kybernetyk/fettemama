c0a95cbb014e5c70d6837d6e082edb66ed96baba
jsz <jszpilewski@me.com>
2011-04-17 12:01:13 +0200

    restart script

---
+++ restart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# warning: this restart script is custom to my server 
+# (the blog runs behind an apache that needs to be restarted too)
+
+echo "telnet blog"
+killall telblog
+cd tnt 
+./telblog > /dev/null &
+cd ..
+
+echo "web blog"
+killall blog
+apache2ctl restart
+cd web
+./blog > /dev/null &
+cd ..
+
+echo "done"
