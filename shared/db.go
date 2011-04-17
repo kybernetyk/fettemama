b02b4a3a88edab6872afa792d196d75f0714832e
jsz <jszpilewski@me.com>
2011-04-17 12:38:09 +0200

    gofmt

--- shared/db.go
+++ shared/db.go
@@ -1,4 +1,5 @@
 package main
+
 import "os"
 
 type BlogDB interface {
@@ -9,8 +10,7 @@ type BlogDB interface {
 	GetPost(post_id int64) (BlogPost, os.Error)
 	GetPostsForTimespan(start_timestamp, end_timestamp, order int64) (posts []BlogPost, err os.Error)
 	GetLastNPosts(num_to_get int32) (posts []BlogPost, err os.Error)
-	
-	StoreComment(comment *PostComment) (int64, os.Error)
-    GetComments(post_id int64) (comments []PostComment, err os.Error);
-}
 
+	StoreComment(comment *PostComment) (int64, os.Error)
+	GetComments(post_id int64) (comments []PostComment, err os.Error)
+}
