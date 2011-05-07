bf65ce3dcad98de492455422c41461b86bd964be
jsz <jszpilewski@me.com>
2011-05-07 17:05:24 +0200

    patrh

--- tnt/main.go
+++ tnt/main.go
@@ -9,7 +9,7 @@ func main() {
 	DBConnect()
 	defer DBDisconnect()
 
-	f, err := os.OpenFile("../logs/telnet.log", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	f, err := os.OpenFile("../../logs/telnet.log", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
 	if err == nil {
 		defer f.Close()
 		log.SetOutput(f)
